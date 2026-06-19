6a7cda0edb08e8963fe8e0eab29bed26d5d634ff
Marco Bodrato <bodrato@mail.dm.unipi.it>
2026-06-19 07:35:09 +0200

    tune: Copyright years

--- tune/speed.h
+++ tune/speed.h
@@ -1,7 +1,7 @@
 /* Header for speed and threshold things.
 
-Copyright 1999-2003, 2005, 2006, 2008-2017, 2019-2022 Free Software
-Foundation, Inc.
+Copyright 1999-2003, 2005, 2006, 2008-2017, 2019-2022, 2026 Free
+Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
